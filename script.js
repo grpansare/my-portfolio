cfed1678ef9a1b0c78788bd3089d6b7d65690af1
grpansare <grpansare2002@gmail.com>
2024-01-21 16:11:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+ScrollReveal({ 
+    reset: true,
+    distance:'80px',
+    duration:1500,
+    delay:200
+
+});
+ScrollReveal().reveal('.header-text,.subtitle,.title', { origin: 'top' });
+ScrollReveal().reveal('.image,.col-7,.project', { origin: 'bottom' });
+// ScrollReveal().reveal('.header-text h1,.contact-left', { origin: 'left' });
+// ScrollReveal().reveal('.contact-right', { origin: 'right' });
+
+
+const typed=new Typed('.multiple-text',{
+   strings :['Front-end developer','Back-end Developer','Full Stack Developer'],
+   typeSpeed:100,
+   backSpeed:10,
+   backDelay:80,
+   loop:true,
+
+})
+
